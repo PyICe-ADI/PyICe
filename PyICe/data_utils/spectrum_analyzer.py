fbb98596f49eda2e52934596638b88902aecbce8
Steve Martin ADI <steve.martin@analog.com>
2023-10-06 11:51:15 -0400

    Added get_record_duration() and get_record_length()

--- PyICe/data_utils/spectrum_analyzer.py
+++ PyICe/data_utils/spectrum_analyzer.py
@@ -9,10 +9,14 @@ class spectrum_analyzer():
     def dBV(self, values):
         return [20*numpy.log10(value) for value in values]
     def compute_fft(self, signal):
-        time, value = zip(*signal)    
-        npoints = len(value)
-        self.yf = self.dBV(2.0 / npoints * numpy.abs((fft(value))[0:npoints//2]))
-        self.xf = fftfreq(npoints, time[1]-time[0])[:npoints//2] # Reconstruct time step, *** THEY ALL NEED TO BE THE SAME ***
+        self.times, self.values = zip(*signal)    
+        self.npoints = len(self.values)
+        self.yf = self.dBV(2.0 / self.npoints * numpy.abs((fft(self.values))[0:self.npoints//2]))
+        self.xf = fftfreq(self.npoints, self.times[1]-self.times[0])[:self.npoints//2] # Reconstruct time step, *** THEY ALL NEED TO BE THE SAME ***
         return self.xf, self.yf
     def get_RBW(self):
         return self.xf[1]-self.xf[0]
+    def get_record_duration(self):
+        return self.times[-1]-self.times[0]
+    def get_record_length(self):
+        return self.npoints
