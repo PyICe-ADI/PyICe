3113d4766651c00bda93e2ea8ae2174032209835
ANALOG\RMcFadde <russell.mcfadden@analog.com>
2023-10-31 09:02:52 -0400

    Workaround for whether an stdf has every test named or just the first pir-prr set.

--- PyICe/refid_modules/correlation/parser.py
+++ PyICe/refid_modules/correlation/parser.py
@@ -1,6 +1,5 @@
 from pystdf.IO import Parser
 import pystdf.V4
-import csv
 
 RECORDTYPE = 0
 
@@ -33,23 +32,41 @@ class STDFParser:
             p.parse()
             self.parts = {}
             self.metadata = {}
+            count=0
+            count0=0
+            stopnow=False
+            test_num_dict = {}
             for line in reader.data:
                 master_info = map_data(*line)
                 record_type = type(line[RECORDTYPE])
                 if record_type is pystdf.V4.Ptr:  # Parametric Test Record - This is a test within this part.
-                    self._test_name_dict[master_info['TEST_TXT']] = {
-                        'upper_limit': master_info['HI_LIMIT'],
-                        'lower_limit': master_info['LO_LIMIT'],
-                        'result': master_info['RESULT'],
-                        'units': master_info['UNITS'],
-                    }
+                    if master_info["TEST_TXT"] != '':
+                        test_num_dict[master_info['TEST_NUM']] = {
+                            'testname': master_info["TEST_TXT"],
+                            'units': master_info['UNITS'],
+                        }
+                        self._test_name_dict[master_info["TEST_TXT"]] = {
+                            'result': master_info['RESULT'],
+                            'units': test_num_dict[master_info['TEST_NUM']]['units'],
+                        }
+                    else:
+                        self._test_name_dict[test_num_dict[master_info['TEST_NUM']]['testname']] = {
+                            'result': master_info['RESULT'],
+                            'units': test_num_dict[master_info['TEST_NUM']]['units'],
+                        }
+                if record_type is pystdf.V4.Prr:
+                    if stopnow:
+                        break
+                    stopnow=True
+
 
     def __getitem__(self, item):
         return self._test_name_dict[item]
 
         # assert num_devices == 1, "Correlation STDF must have only one DUT record"
 
-if __name__ == '__main__':
-    # STDFParser(filename='example_stdf/lot2.stdf')
-    STDFParser(filename='../../../../../projects/stowe_eval/correlation/REVID7/2022-01-05/5627908_LT3390_25C_CLASS_PRI_FT_TRIM_LT3390_BOS-EAGLE1_20220105_102154.std_1')
 
+if __name__ == '__main__':
+    stuff = STDFParser(filename='example_stdf/lot2.stdf')
+    #stuff = STDFParser(filename='../../../../../projects/stowe_eval/correlation/REVID7/2023-04-14/ENG_LT3390-6J_25C_ENG_ENG_FT_TRIM_LT3390_ETS1UOJU4-00334_20230414_162608.std_1')
+    breakpoint()
