1dbf5401e55f78766594119d89de77d5e9c7fadc
ANALOG\RMcFadde <russell.mcfadden@analog.com>
2023-11-02 17:01:28 -0400

    Gets part ids from stdf files as well, now.

--- PyICe/refid_modules/correlation/correlation_utils.py
+++ PyICe/refid_modules/correlation/correlation_utils.py
@@ -5,7 +5,7 @@ class CorrelationUtils:
     def __init__(self):
         self.index_file = "file/location.csv"
 
-    def get_stdf_location(self, dut_id):
+    def get_stdf_info(self, dut_id):
         """
         From a csv file that contains rows of identifying names of duts and the stdf file location that has the dut's
         ATE test data, return the location.
@@ -19,27 +19,23 @@ class CorrelationUtils:
             csvreader = csv.reader(index_file)
             for row in csvreader:
                 if row[0] is dut_id:
-                    stdf_file = row[0]
+                    stdf_file = row[1]
+                    part_ids = row[2] # or hash or something.
                     break
-        return stdf_file
+        target_data_dict = STDFParser(stdf_file, part_ids)
+        return target_data_dict
 
-    def append_stdf_index(self, dut_id, stdf_file_location):
+    def append_stdf_index(self, dut_id, stdf_file_location, part_id):
         """
         Adds a new line to the existing index.
         Args:
             dut_id: Unique name of the dut with which ATE data was collected.
             stdf_file_location: Location of the stdf file that has the ATE data collected.
+            part_id: The value of the 'PART_ID' in the PRR of the dut in question.
 
         Returns:
         N/A
         """
         with open(self.index_file, 'a') as index_file:
             csvwriter = csv.writer(index_file)
-            csvwriter.writerow([dut_id, stdf_file_location])
-
-    def consolidate_duts(self):
-        '''I think an stdf file is only going to have one temperature in it,
-        so we could have three different files for one DUT. Need to think 
-        about how to determine which file we want, or if we want to merge 
-        them all somehow.'''
-        pass
+            csvwriter.writerow([dut_id, stdf_file_location, part_id])
