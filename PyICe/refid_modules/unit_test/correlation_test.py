888647f581f25154f2dd9cb7316ecfced984d647
Tim Laracy <Tim.Laracy@analog.com>
2023-11-03 12:01:21 -0400

    Adding some test cases

--- PyICe/refid_modules/unit_test/correlation_test.py
+++ PyICe/refid_modules/unit_test/correlation_test.py
@@ -2,8 +2,9 @@ import pytest
 from PyICe.refid_modules.correlation.correlation_analyzer import CorrelationAnalyzer
 from PyICe.refid_modules.correlation.parser import STDFParser
 
-corr_ex = ['../correlation/example_stdf/lot2.stdf']
-stwe_ex = ['../../../../../projects/stowe_eval/correlation/REVID7/2022-01-05/5627908_LT3390_25C_CLASS_PRI_FT_TRIM_LT3390_BOS-EAGLE1_20220105_102154.std_1']
+corr_ex = ['correlation/example_stdf/lot2.stdf']
+# stwe_ex = ['../../../../../projects/stowe_eval/correlation/REVID7/2022-01-05/5627908_LT3390_25C_CLASS_PRI_FT_TRIM_LT3390_BOS-EAGLE1_20220105_102154.std_1']
+stwe_ex = ['correlation/example_stdf/5627908_LT3390_25C_CLASS_PRI_FT_TRIM_LT3390_BOS-EAGLE1_20220105_102154.std_1']
 
 
 @pytest.mark.parametrize('filename, part_id', [(corr_ex, ['2']), (stwe_ex, ['1'])])
@@ -48,8 +49,30 @@ def test_set_limits(filename, part_id, testname, kwargs):
                                                         'upper_diff': 0.1, 'lower_diff': None}),
                                              (stwe_ex, ['1'], {'testname': 'VOUT CH1', 'bench_data': [0.8], 'units': 'V',
                                                         'percent': 3}),
+                                             (stwe_ex, '1', {'testname': 'VOUT CH1', 'bench_data': 0.8, 'units': 'V',
+                                                           'upper_diff': 0.1, 'lower_diff': -0.1, 'temperature': 25}),
+                                             (stwe_ex, '1', {'testname': 'VOUT CH1', 'bench_data': 0.8, 'units': 'V',
+                                                           'upper_diff': 0.1, 'lower_diff': -0.1, 'temperature': '25'}),
+                                             (stwe_ex, '2', {'testname': 'VOUT CH1', 'bench_data': 0.8, 'units': 'V',
+                                                           'upper_diff': 0.1, 'lower_diff': -0.1, 'temperature': '25'})
                                              ]
                          )
 def test_verdict(filename, part_id, kwargs):
     analyzer = CorrelationAnalyzer(STDFParser(filename, part_id))
     assert analyzer.verdict(**kwargs)
+@pytest.mark.parametrize('filename, part_id, kwargs', [(corr_ex, ['2'],
+                                                        {'testname': 'Src out I       <> EA_SRC', 'bench_data': [1.3],
+                                                         'units': 'A', 'upper_diff': 1, 'lower_diff': -1}),
+                                                       (stwe_ex, ['1'], {'testname': 'VOUT CH1', 'bench_data': 0, 'units': 'V',
+                                                                         'upper_diff': 0.1, 'lower_diff': -0.1}),
+                                                       (stwe_ex, ['1'], {'testname': 'VOUT CH1', 'bench_data': [1000], 'units': 'mV',
+                                                                         'upper_diff': 0.1, 'lower_diff': None}),
+                                                       (stwe_ex, '1', {'testname': 'VOUT CH1', 'bench_data': 1.2, 'units': 'V',
+                                                                       'upper_diff': 0.1, 'lower_diff': -0.1, 'temperature': 25}),
+                                                       (stwe_ex, '2', {'testname': 'VOUT CH1', 'bench_data': 1.2, 'units': 'V',
+                                                                       'upper_diff': 0.1, 'lower_diff': -0.1, 'temperature': '25'})
+                                                       ]
+                         )
+def test_verdict_fail(filename, part_id, kwargs):
+    analyzer = CorrelationAnalyzer(STDFParser(filename, part_id))
+    assert not analyzer.verdict(**kwargs)
