b1eacb11c74066343f8b8cbf7bbdca6cc3d746c7
ANALOG\RMcFadde <russell.mcfadden@analog.com>
2025-01-21 08:10:32 -0500

    String typo.

--- PyICe/lab_utils/banners.py
+++ PyICe/lab_utils/banners.py
@@ -9,7 +9,7 @@ def build_banner(*message, offset=1, length=80):
     lower_right = u"\u2518"
     wall = u"\u2502"
     
-    ret_str=f'{upper_left}{bar * (length-2)}{upper_right}\n"
+    ret_str=f'{upper_left}{bar * (length-2)}{upper_right}\n'
     for line in message:
         ret_str+=f'{wall}{" " * offset}{line}{" " * ((length-2-offset) - len(line))}{wall}\n'
     ret_str+=f'{lower_left}{bar * (length-2)}{lower_right}'
