417e6b7dc71c444cfbbeb1542d8f4492057ffab9
ANALOG\RMcFadde <russell.mcfadden@analog.com>
2025-03-20 14:34:53 -0400

    Registering a test failure would crash the test if the refid that the user was failing hadn't submitted any data previously because "How can we add the failure data point to the test's repository if the repository hadn't been made?" Make the repository. It's that simple.

--- PyICe/plugins/test_results.py
+++ PyICe/plugins/test_results.py
@@ -310,7 +310,8 @@ class Test_Results(generic_results):
 
     def _register_test_failure(self, name, reason, conditions, query=None):
         if name not in self._test_declarations:
-            raise Exception(f'Undeclared test results: {name}')
+            self._test_declarations.append(name)
+            self._test_results[name] = self._test_results_list(name=name, upper_limit=self.test_limits[name]['upper_limit'], lower_limit=self.test_limits[name]['lower_limit'], override=self._failure_override)
         failure_result = self._test_result(test_name=name,
                                            conditions=conditions,
                                            min_data=None,
