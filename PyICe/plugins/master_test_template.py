6d01580b32b79d90e7afa547c6c3b9049591291d
ANALOG\RMcFadde <russell.mcfadden@analog.com>
2025-08-19 14:44:49 -0400

    The evaluate and correlate methods now have a (default empty dictionary) parameter called limits that a user can provide to bypass a project's (previously required) get_test_limits method.

--- PyICe/plugins/master_test_template.py
+++ PyICe/plugins/master_test_template.py
@@ -81,39 +81,58 @@ class Master_Test_Template():
         '''Optional evaluate_results method placeholder'''
     def correlate_results(self):
         '''Optional correlate_results method placeholder'''
-    def evaluate_rawdata(self, name, data, conditions=None):
+    def evaluate_rawdata(self, name, data, conditions=None, limits:dict={}):
         '''This will compare submitted data to limits for the named test.
         args:
             name - string. The name of the test whose limits will be used.
             data - Boolean or iterable object. Each value will be compared to the limits (or boolean value) of the name argument.
-            conditions - None or dictionary. A dictionary with channel names as keys and channel values as values. Used to report under what circumstances the data was taken. Default is None.'''
-        self._test_results.test_limits[name]=self.get_test_limits(name)
+            conditions - None or dictionary. A dictionary with channel names as keys and channel values as values. Used to report under what circumstances the data was taken. Default is None.
+            limits - Dictionary. A dictionary with the value limits to compare to the data. Must provide at least an upper or a lower limit as a key. If none are provided, the project's get_test_limits method will be used.'''
+        if len(limits) is 0:
+            self._test_results.test_limits[name]=self.get_test_limits(name)
+        elif 'upper_limit' in limits.keys() or 'lower_limit' in keys():
+            self._test_results.test_limits[name]=limits
+        else:
+            raise KeyError(f"The limits argument for evaluate_rawdata requires an upper_limit and/or a lower_limit in its keys.\nFound {limits.keys()}.")
         self._test_results._evaluate_list(name=name, iter_data=data, conditions=conditions)
-    def evaluate_query(self, name, query):
+    def evaluate_query(self, name, query, limits:dict={}):
         '''This will compare submitted data to limits for the named test.
         args:
             name - string. The name of the test whose limits will be used.
-            database - SQLite database object. The first column will be compared to the limits of the named spec and the rest will be used for grouping.'''
-        self._test_results.test_limits[name]=self.get_test_limits(name)
+            database - SQLite database object. The first column will be compared to the limits of the named spec and the rest will be used for grouping.
+            limits - dictionary. A dictionary with the value limits to compare to the data obtained by the query. Must provide at least an upper or a lower limit as a key. If none are provided, the project's get_test_limits method will be used.'''
+        if len(limits) is 0:
+            self._test_results.test_limits[name]=self.get_test_limits(name)
+        elif 'upper_limit' in limits.keys() or 'lower_limit' in keys():
+            self._test_results.test_limits[name]=limits
+        else:
+            raise KeyError(f"The limits argument for evaluate_query requires an upper_limit and/or a lower_limit in its keys.\nFound {limits.keys()}.")
         self.get_database().query(query)
         self._test_results._evaluate_database(name=name, database=self.get_database())
-    def evaluate_db(self, name):
+    def evaluate_db(self, name, limits:dict={}):
         '''This method evaluates a pre-massaged SQLite database, self.get_database(), from the user. It returns a bit of flexibility on the sequel query to the user.
         args:
-            name - string. The name of the test whose limits will be used.'''
-        self._test_results.test_limits[name]=self.get_test_limits(name)
+            name - string. The name of the test whose limits will be used.
+            limits - dictionary. A dictionary with the value limits to compare to the data obtained from the database. Must provide at least an upper or a lower limit as a key. If none are provided, the project's get_test_limits method will be used.'''
+        if len(limits) is 0:
+            self._test_results.test_limits[name]=self.get_test_limits(name)
+        elif 'upper_limit' in limits.keys() or 'lower_limit' in keys():
+            self._test_results.test_limits[name]=limits
+        else:
+            raise KeyError(f"The limits argument for evaluate_db requires an upper_limit and/or a lower_limit in its keys.\nFound {limits.keys()}."))
         self._test_results._evaluate_database(name=name, database=self.get_database())
-    def evaluate(self, name, values, conditions=[], where_clause=''):
+    def evaluate(self, name, values, conditions=[], where_clause='', limits:dict={}):
         '''This compares submitted data from a SQLite database to a named test in a more outlined fashion.
         args:
             name - string. The name of the test with limits to be used.
             value_column - string. The name of the channel that will be evaluated.
-            grouping_columns - list. The values of the value_column will be grouped and evaluated by the permutations of the channels that are named in this list of strings.'''
+            grouping_columns - list. The values of the value_column will be grouped and evaluated by the permutations of the channels that are named in this list of strings.
+            limits - dictionary. A dictionary with the value limits to compare to the data obtained from the database. Must provide at least an upper or a lower limit as a key. If none are provided, the project's get_test_limits method will be used.'''
         condition_str = ''
         for condition in conditions:
             condition_str += f",{condition}"
         query_str = f'SELECT {values}{condition_str} FROM {self.get_table_name()} ' + ('WHERE ' + where_clause if where_clause else '')
-        self.evaluate_query(name, query=query_str)
+        self.evaluate_query(name, query=query_str, limits = limits)
     def register_test_failure(self, name, reason, conditions=None, query=None):
         '''Submit a result for a test that is considered a FAIL so the test, regardless of other data submitted, will result in a FAIL overall.'''
         self._test_results._register_test_failure(name=name, reason=reason, conditions=conditions, query=query)
@@ -143,7 +162,7 @@ class Master_Test_Template():
                         reported_condition.append(row[recon])
                     self.register_test_failure(name=name, reason=f"Channel {excon} was found to be False. ", conditions=reported_condition)
         
-    def correlate_data(self, name, reference_values=[], test_values=[], spec=None, conditions=None):
+    def correlate_data(self, name, reference_values=[], test_values=[], spec=None, conditions=None, limits:dict={}):
         '''Compares test values to reference values and compare the output to the limits of the named test.
         args:
             name - string. The name of the test whose limits will be used.
@@ -151,7 +170,12 @@ class Master_Test_Template():
             test_values - iterable. The object values whose distance to the reference value will be calculated.
             spec - string. Either '%' or '-'. Determines whether the comparison is made by percentage or by difference.
             conditions - None or dictionary. A dictionary with channel names as keys and channel values as values. Used to report under what circumstances the data was taken. Default is None.'''
-        self._corr_results.test_limits[name]=self.get_test_limits(name)
+        if len(limits) is 0:
+            self._corr_results.test_limits[name]=self.get_test_limits(name)
+        elif 'upper_limit' in limits.keys() or 'lower_limit' in keys():
+            self._corr_results.test_limits[name]=limits
+        else:
+            raise KeyError(f"The limits argument for correlate_data requires an upper_limit and/or a lower_limit in its keys.\nFound {limits.keys()}.")
         self._corr_results._correlate_results(name=name, reference_values=reference_values, test_values=test_values, spec=spec, conditions=conditions)
     def get_test_results(self):
         '''Returns a string that reports the Pass/Fail status for all the tests evaluated in the script and the test script as a whole.'''
