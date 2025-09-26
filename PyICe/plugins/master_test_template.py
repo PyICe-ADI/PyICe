a89da889496a5394dd7edb0a0409c1c01cb9743b
ANALOG\RMcFadde <russell.mcfadden@analog.com>
2025-09-26 00:04:21 -0400

    Break out upper and lower limits from the optional declarations, since they are not.

--- PyICe/plugins/master_test_template.py
+++ PyICe/plugins/master_test_template.py
@@ -81,21 +81,18 @@ class Master_Test_Template():
         '''Optional evaluate_results method placeholder'''
     def correlate_results(self):
         '''Optional correlate_results method placeholder'''
-    def declare_test(self, name:str, decl:dict={}):
+    def declare_test(self, name:str, lower_limit, upper_limit, **kwargs):
         '''Optional means to manually set declarations apart from the evaluation methods.'''
         established_declarations = self._test_results.test_limits
-        if len(decl.keys()) == 0:
-            if name not in established_declarations.keys():
-                established_declarations[name]=self.get_test_limits(name)
-            else:
-                assert established_declarations[name]==self.get_test_limits(name), f"***Master Test Template Error*** Trying to change {name}'s declarations from {established_declarations[name]} to {self.get_test_limits(name)}"
-        elif 'upper_limit' not in decl or 'lower_limit' not in decl:
-            raise Exception(f"Declarations must include at least an upper and a lower limit. Received {decl}")
+        if name not in established_declarations.keys():
+            established_declarations[name]=self.get_test_limits(name)
+        else:
+            assert established_declarations[name]==self.get_test_limits(name), f"***Master Test Template Error*** Trying to change {name}'s declarations from {established_declarations[name]} to {self.get_test_limits(name)}"
         else:
             if name not in established_declarations.keys():
-                established_declarations[name]=decl
+                established_declarations[name]={'lower_limit':lower_limit, 'upper_limit':upper_limit, **kwargs}
             else:
-                assert established_declarations[name]==decl, f"***Master Test Template Error*** Trying to change {name}'s declarations from {established_declarations[name]} to {decl}"
+                assert established_declarations[name]=={'lower_limit':lower_limit, 'upper_limit':upper_limit, **kwargs}, f"***Master Test Template Error*** Trying to change {name}'s declarations from {established_declarations[name]} to {decl}"
     def evaluate_rawdata(self, name, data, conditions=None):
         '''This will compare submitted data to limits for the named test.
         args:
@@ -103,7 +100,7 @@ class Master_Test_Template():
             data - Boolean or iterable object. Each value will be compared to the limits (or boolean value) of the name argument.
             conditions - None or dictionary. A dictionary with channel names as keys and channel values as values. Used to report under what circumstances the data was taken. Default is None.'''
         if name not in self._test_results.test_limits.keys():
-            self.declare_test(name)
+            self.declare_test(name, **self.get_test_limits(name))
         self._test_results._evaluate_list(name=name, iter_data=data, conditions=conditions)
     def evaluate_query(self, name, query):
         '''This will compare submitted data to limits for the named test.
@@ -111,7 +108,7 @@ class Master_Test_Template():
             name - string. The name of the test whose limits will be used.
             database - SQLite database object. The first column will be compared to the limits of the named spec and the rest will be used for grouping.'''
         if name not in self._test_results.test_limits.keys():
-            self.declare_test(name)
+            self.declare_test(name, **self.get_test_limits(name))
         self.get_database().query(query)
         self._test_results._evaluate_database(name=name, database=self.get_database())
     def evaluate_db(self, name):
@@ -119,7 +116,7 @@ class Master_Test_Template():
         args:
             name - string. The name of the test whose limits will be used.'''
         if name not in self._test_results.test_limits.keys():
-            self.declare_test(name)
+            self.declare_test(name, **self.get_test_limits(name))
         self._test_results._evaluate_database(name=name, database=self.get_database())
     def evaluate(self, name, values, conditions=[], where_clause=''):
         '''This compares submitted data from a SQLite database to a named test in a more outlined fashion.
