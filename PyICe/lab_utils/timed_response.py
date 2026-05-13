d764f6bbb8befbcb4c12a42ec28dba2c69a1e2c2
ANALOG\RMcFadde <russell.mcfadden@analog.com>
2026-05-13 08:27:30 -0400

    Add a time limit for the user to submit a response to a given prompt. Returns None if the timer expires.

---
+++ PyICe/lab_utils/timed_response.py
@@ -0,0 +1,14 @@
+import threading
+
+def timed_input(prompt, timeout):
+    '''Prompt the user for input, returning None if no response within timeout seconds.'''
+    response = [None]
+    def _get_input():
+        response[0] = input(prompt)
+    thread = threading.Thread(target=_get_input, daemon=True)
+    thread.start()
+    thread.join(timeout)
+    if thread.is_alive():
+        print(f"\nNo response received within {timeout}s. Continuing...")
+        return None
+    return response[0]
